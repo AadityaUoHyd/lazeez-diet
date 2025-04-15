57372304a92dbdb0c371bb2813ce1c5754822990
AadityaUoHyd <aadiraj48@gmail.com>
2025-04-15 07:48:06 +0530

    vite-env.d.ts addded

--- client/src/vite-env.d.ts
+++ client/src/vite-env.d.ts
@@ -1,1 +1,11 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+    readonly VITE_API_URL: string; // Replace/add your actual env vars here
+    // Add more if needed
+  }
+  
+  interface ImportMeta {
+    readonly env: ImportMetaEnv;
+  }
+  /// <reference types="vite/client" />
